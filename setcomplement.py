fdc92924de1de7d4994520a257a1faa52507cec1
Aaron Hall, MBA <aaronchall@yahoo.com>
2015-10-02 17:10:19 -0400

    Update setcomplement.py

--- setcomplement.py
+++ setcomplement.py
@@ -2,6 +2,11 @@ from collections import MutableSet
 
 
 class SetComplement(MutableSet):
+    
+    #def __new__(cls, s):
+        #'''TODO: make this make sense for a complement of a complement'''
+        #return super(SetComplement, cls).__new__(s)
+    
     def __init__(self, s):
         self.set = set(s)
 
@@ -9,11 +14,17 @@ class SetComplement(MutableSet):
         return e not in self.set
 
     def __iter__(self):
+        '''
+        since iteration order is arbitrary, could just iterate over everything in memory
+        not in the self.set, then start 
+        iterating over a itertools.count(max_int_size_in_memory)
+        '''
         raise NotImplementedError('figure out how to iterate over everything not in the self.set')
 
     def __len__(self):
         '''if self.set is not infinite (Python limitation), perhaps we always are?'''
-        return float('inf') # raises an overflow error on attempt to convert to intq
+        # TODO: subclass int and create an `inf` for it. probably just use float('inf') for comparisons
+        return float('inf') # raises an overflow error on attempt to convert to int
 
     def add(self, e):
         self.set.discard(e)
